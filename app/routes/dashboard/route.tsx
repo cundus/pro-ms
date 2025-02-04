9775c639e397a2789953a558b482fceb6daf7279
Sundus <pcinta0@gmail.com>
2025-02-04 20:29:26 +0700

    setting up config

--- app/routes/dashboard/route.tsx
+++ app/routes/dashboard/route.tsx
@@ -1,11 +1,14 @@
-import Button from "~/components/Button";
+import { Outlet } from '@remix-run/react'
+
+import Button from '~/components/Button'
 
 const Dashboard = () => {
   return (
     <div>
       <Button />
+      <Outlet />
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
