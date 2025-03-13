5dfe2df7ac0722f1f0100b0cc17d8898369fb5dc
cundus <pcint0@gmail.com>
2025-03-13 20:15:45 +0700

    init role files

---
+++ app/routes/dashboard/master+/role/_index/index.tsx
@@ -0,0 +1,10 @@
+import Heading from '~/components/heading'
+
+export default function Index() {
+  return (
+    <>
+      <Heading title="Role Management" />
+      <div className="flex items-center justify-between my-4"></div>
+    </>
+  )
+}
---
+++ app/routes/dashboard/master+/role/_layout.tsx
@@ -0,0 +1,28 @@
+import { MetaFunction } from '@remix-run/node'
+import { Navigate, Outlet, useOutletContext } from '@remix-run/react'
+
+import Container from '~/components/Container'
+import type { IOutletContext } from '~/types/outlet-context'
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      title: 'Role Management',
+    },
+  ]
+}
+
+const RoleLayout = () => {
+  const outletContext = useOutletContext<IOutletContext>()
+
+  if (!outletContext.userSession.role?.is_global) {
+    return <Navigate to="/dashboard" />
+  }
+  return (
+    <Container>
+      <Outlet />
+    </Container>
+  )
+}
+
+export default RoleLayout
