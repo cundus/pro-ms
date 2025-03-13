8d6faae0347874f17ce486853f0b3a25bfd86ae4
fahmi <fahmi.mellow@gmail.com>
2025-03-13 19:57:04 +0700

    chore/ : add is_active field

--- app/types/menu.d.ts
+++ app/types/menu.d.ts
@@ -1,9 +1,13 @@
 export interface IMenu
-  extends Record<string, string | number | undefined | null | IMenu[]> {
+  extends Record<
+    string,
+    string | number | boolean | undefined | null | IMenu[]
+  > {
   id?: number | null
   label: string
   icon?: string | null
   path?: string | null
   parent_id?: number | null
+  is_active?: boolean
   children?: IMenu[] | null
 }
