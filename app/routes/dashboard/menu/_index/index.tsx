89c938fc03c5779c5919a015dd2f14195648d311
fahmi <fahmi.mellow@gmail.com>
2025-03-17 20:24:15 +0700

    chore/minor adjustment

--- app/routes/dashboard/menu/_index/index.tsx
+++ app/routes/dashboard/menu/_index/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
 import { Menu } from '@prisma/client'
+import { LoaderFunction } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 
 import { columns } from '~/components/data-table/columns'
-import Table from '~/components/data-table/tables'
+import CommonTable from '~/components/data-table/tables'
 import Heading from '~/components/heading'
+import { getAllMenus } from '~/repositories/menu.server'
 import { Columns } from '~/types/columns'
+import { guard } from '~/utils/guard.server'
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await guard(request)
+  const menus = await getAllMenus()
+  return Response.json({ menus })
+}
 
 function Index() {
-  const [menus, setMenus] = React.useState([])
+  const { menus } = useLoaderData<typeof loader>()
+
   const column: Columns[] = [
     {
       key: 'label',
@@ -35,25 +45,15 @@ function Index() {
     },
   ]
 
-  React.useEffect(() => {
-    const fetchMenus = async () => {
-      try {
-        const response = await fetch('/api/menus/all')
-        const data = await response.json()
-        setMenus(data.menus)
-      } catch (error) {
-        console.error('Error fetching menus:', error)
-      }
-    }
-
-    fetchMenus()
-  }, [])
-
   return (
     <>
       <Heading title="Menu Management" />
       <div className="flex items-center justify-between my-4"></div>
-      <Table columns={columns<Menu>(column)} data={menus} searchBy="label" />
+      <CommonTable
+        columns={columns<Menu>(column)}
+        data={menus}
+        searchBy="label"
+      />
     </>
   )
 }
