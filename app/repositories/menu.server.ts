578fff6a0d5b86e2de7a8e3e9609f913a4cb059e
fahmi <fahmi.mellow@gmail.com>
2025-03-17 20:20:37 +0700

    feat/add getall, getbyid, create, update, delete menu repo

--- app/repositories/menu.server.ts
+++ app/repositories/menu.server.ts
@@ -1,5 +1,7 @@
 import { prisma } from '../lib/db.server'
 
+import { MenuBody } from '~/types/menu'
+
 export const getMenuAndPermissions = async (
   userRoleId: number,
   isGlobal: boolean
@@ -45,6 +47,30 @@ export const getMenuAndPermissions = async (
   return menus
 }
 
-export const getAllMenus = async () => {
-  return await prisma.menu.findMany({ orderBy: { id: 'desc' } })
+type OrderParams = {
+  [key: string]: string
+}
+
+export const getAllMenus = async (orderBy: OrderParams = { id: 'desc' }) => {
+  return await prisma.menu.findMany({ orderBy })
+}
+
+export const countMenuById = async (id: number) => {
+  return await prisma.menu.count({ where: { id, is_active: true } })
+}
+
+export const getMenuById = async (id: number) => {
+  return await prisma.menu.findUnique({ where: { id } })
+}
+
+export const createMenu = async (body: MenuBody) => {
+  return await prisma.menu.create({ data: body })
+}
+
+export const updateMenu = async (id: number, body: MenuBody) => {
+  return await prisma.menu.update({ where: { id }, data: body })
+}
+
+export const deleteMenu = async (id: number) => {
+  return await prisma.menu.delete({ where: { id } })
 }
