1b4fc8a94c9bc81c3f061a9b817c4bf239d11682
fahmi <fahmi.mellow@gmail.com>
2025-03-17 20:27:44 +0700

    feat/add edit menu page

---
+++ app/routes/dashboard/menu/edit.$id/index.tsx
@@ -0,0 +1,49 @@
+import {
+  ActionFunctionArgs,
+  LoaderFunction,
+  MetaFunction,
+} from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
+
+import MenuPage from '../_components/page'
+
+import Back from '~/components/data-table/components/back-button'
+import Heading from '~/components/heading'
+import { getAllMenus, getMenuById } from '~/repositories/menu.server'
+import { editMenuService } from '~/services/menu.server'
+import { guard } from '~/utils/guard.server'
+
+export const meta: MetaFunction = () => {
+  return [
+    {
+      title: 'Edit Menu',
+    },
+  ]
+}
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  return await editMenuService(request)
+}
+
+export const loader: LoaderFunction = async ({ request, params }) => {
+  await guard(request)
+  const menus = await getAllMenus({ label: 'asc' })
+  const data = await getMenuById(Number(params.id))
+  return Response.json({ menus, data })
+}
+
+const Index = () => {
+  const { menus, data } = useLoaderData<typeof loader>()
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <Heading title="Edit Menu" />
+        <Back />
+      </div>
+      <MenuPage page="edit" menus={menus} data={data} />
+    </>
+  )
+}
+
+export default Index
